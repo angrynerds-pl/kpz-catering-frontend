a37c05aeb0fb10074b0af5f3f3e0423e7d5da8d8
Oskar <podgrzyb123@gmail.com>
2020-05-19 18:53:57 +0200

    changed url

--- src/app/admin/services/login.service.ts
+++ src/app/admin/services/login.service.ts
@@ -11,7 +11,7 @@ import { User } from 'src/app/admin/model/User';
 export class LoginService {
   private currentUserSubject: BehaviorSubject<User>;
   public currentUser: Observable<User>;
-  url = 'https://cateringbackend.azurewebsites.net';
+  url = 'https://cateringbackend.azurewebsites.net/admin/authenticate';
   
   constructor(private http: HttpClient) { 
     this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
@@ -22,7 +22,7 @@ export class LoginService {
 }
 
 login(username: string, password: string) {
-    return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username, password })
+    return this.http.post<any>(this.url, { username, password })
         .pipe(map(user => {
             // store user details and jwt token in local storage to keep user logged in between page refreshes
             localStorage.setItem('currentUser', JSON.stringify(user));
